ffdd4134e122bb4a6c555d9e1756ae260f1a3519
sakshi yadav <sakshi923456@gmail.com>
2021-08-30 15:35:50 +0530

    components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-   <>
-   <h1>This Proshop</h1>
-   </>
+    <>
+      <Header />
+      <main>
+        {" "}
+        <h1>This Proshop</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// rafce for arrow func and rfc for normal function
+
+const Header = () => {
+    return (
+        <Header>
+            Header
+        </Header>
+    )
+}
+
+export default Header
